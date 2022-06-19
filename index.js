2d55eb2f58d2ad348ec484cd751b1a1b0deeaecd
mdrasel101126 <mdrasel101126@gmail.com>
2022-06-20 01:36:03 +0600

    second Comit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config()
 const cors=require('cors');
-const port=5000;
+const port=process.env.PORT || 5000;
 const app=express();
 app.use(cors());
 app.use(express.json());
